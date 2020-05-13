240b68d015d91d8040dd6488e634e2129a23bf87
wjunnneg <1194348056@qq.com>
2020-05-13 08:50:52 +0800

    update baselines.py

--- src/cores/baselines.py
+++ src/cores/baselines.py
@@ -12,7 +12,7 @@ train = pd.read_csv('../../data/output/train.csv')
 test = pd.read_csv('../../data/output/test.csv')
 model = ClassificationModel('roberta', 'roberta-large', num_labels=11,
                             args={'sliding_window': True, 'reprocess_input_data': True, 'overwrite_output_dir': True,
-                                  'logging_steps': 5, 'train_batch_size': 8, 'gradient_accumulation_steps': 16,
+                                  'logging_steps': 1000, 'train_batch_size': 4, 'gradient_accumulation_steps': 16,
                                   'stride': 0.6, 'max_seq_length': 512, 'num_train_epochs': 4})
 model.train_model(train)
 predictions, _ = model.predict([row['text'] for _, row in test.iterrows()])
