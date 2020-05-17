390dfe18ec4efc4f094cf8b62f38c1d401a876c1
wjunnneg <1194348056@qq.com>
2020-05-17 14:26:24 +0800

    update 90_25.

--- src/cores/baselines.py
+++ src/cores/baselines.py
@@ -14,8 +14,9 @@ model = ClassificationModel('roberta', 'roberta-large', num_labels=11,
                             args={'sliding_window': True, 'reprocess_input_data': True, 'overwrite_output_dir': True,
                                   'logging_steps': 1000, 'train_batch_size': 4, 'gradient_accumulation_steps': 16,
                                   'stride': 0.6, 'max_seq_length': 512, 'num_train_epochs': 4,
-                                  'show_running_loss': False})
-model.train_model(train)
+                                  'show_running_loss': False,
+                                  'cache_dir': "/content/2020-AI-Financial-User-Review-Categories/src/cores/outputs"})
+model.load_and_cache_examples(examples=train)
 predictions, _ = model.predict([row['text'] for _, row in test.iterrows()])
 pd.DataFrame({'ID': test.index, 'label': predictions}).to_csv('../../data/output/keys_longformer.csv', index=False,
                                                               header=False)
